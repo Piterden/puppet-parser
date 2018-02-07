5cda74946dbfe074824a9b82cd0d54785d1bd7d3
Denis Efremov <efremov.a.denis@gmail.com>
2018-02-07 14:05:49 +0300

    Added logging

--- index.js
+++ index.js
@@ -51,14 +51,15 @@ const launchConfig = {
  * @return {{ thumb:string, title:string, link:string, description:string }}
  */
 const coursesMapper = async (courseEl) => {
+  console.log(`[COURSES MAPPER]: ${Object.keys(courseEl)}`)
   const thumbEl = await courseEl.$($thumbEl)
   const titleEl = await courseEl.$($titleEl)
   const descriptionEl = await courseEl.$($descriptionEl)
 
-  const thumb = await thumbEl.getProperty('src')
-  const title = await titleEl.getProperty('innerHTML')
-  const link = await titleEl.getProperty('href')
-  const description = await descriptionEl.getProperty('innerHTML')
+  const thumb = thumbEl && await thumbEl.getProperty('src')
+  const title = titleEl && await titleEl.getProperty('innerHTML')
+  const link = titleEl && await titleEl.getProperty('href')
+  const description = descriptionEl && await descriptionEl.getProperty('innerHTML')
 
   return {
     thumb: await thumb.jsonValue(),
@@ -75,6 +76,7 @@ const coursesMapper = async (courseEl) => {
  * @param {ElementHandle} itemEl
  */
 const collectionsMapper = async (itemEl) => {
+  console.log(`[COLLECTIONS MAPPER]: ${Object.keys(itemEl)}`)
   try {
     const linkEl = await itemEl.$('[href]')
     const link = await linkEl.getProperty('href')
@@ -82,21 +84,36 @@ const collectionsMapper = async (itemEl) => {
     return { link: await link.jsonValue() }
   }
   catch (error) {
+    console.log(itemEl)
     console.log(error)
   }
 
   return false
 }
 
+/**
+ * Determines if the given link is post page
+ *
+ * @param {string} link
+ * @return {boolean}
+ */
 const isPostPage = (link) => link.match(/\/posts\//)
 
+/**
+ * Process a post page
+ *
+ * @param {Page} page The page instance
+ * @param {string} link The next page URL
+ */
 const parseMedia = async (page, link) => {
+  console.log(`[MEDIA PAGE]: ${link}`)
   const parentLink = await page.url()
 
   await page.goto(link)
 
   try {
     const titleEl = await page.$('h1,h2')
+
     const title = await titleEl.getProperty('innerHTML')
 
     videos.push({
@@ -108,7 +125,15 @@ const parseMedia = async (page, link) => {
   }
 }
 
+/**
+ * Process a list page
+ *
+ * @param {Page} page
+ * @param {string} link
+ * @param {{courseIdx:number, level:number, childIdx:number}}
+ */
 const parseCollection = async (page, link, { courseIdx, level = 0, childIdx = 0 }) => {
+  console.log(`[COLLECTION PAGE]: ${{ courseIdx, level, childIdx }}`)
   await page.goto(link)
   const items = await page.$$($courseItemsSelector)
   const urls = await Promise.all(items.map(collectionsMapper).filter(Boolean))
